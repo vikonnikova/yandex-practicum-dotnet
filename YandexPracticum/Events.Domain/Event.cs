d18fcd18d2553295e21cf84d566a203e145d559d
vika <vika_7486@mail.ru>
2026-05-03 22:00:47 +0500

    Domain layer added

---
+++ YandexPracticum/Events.Domain/Event.cs
@@ -0,0 +1,24 @@
+﻿namespace Events.Domain;
+
+public class Event
+{
+	private Event(int id, string title, string? description, DateTime startAt, DateTime endAt)
+	{
+		Id = id;
+		Title = title;
+		Description = description;
+		StartAt = startAt;
+		EndAt = endAt;
+	}
+
+	public int Id { get; private set; }
+	public string Title { get; private set; }
+	public string? Description { get; private set; }
+	public DateTime StartAt { get; private set; }
+	public DateTime EndAt { get; private set; }
+
+	public static Event Create(int id, string title, string? description, DateTime startAt, DateTime endAt)
+	{
+		return new Event(id, title, description, startAt, endAt);
+	}
+}
