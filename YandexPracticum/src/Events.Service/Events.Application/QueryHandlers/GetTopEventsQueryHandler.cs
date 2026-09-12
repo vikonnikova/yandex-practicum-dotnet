9366e8f69d549758e2b0fb0c7b7a8078a9c94ad3
vika <vika_7486@mail.ru>
2026-09-13 01:25:25 +0500

    Добавлен кэш в обработчики запросов.

--- YandexPracticum/src/Events.Service/Events.Application/QueryHandlers/GetTopEventsQueryHandler.cs
+++ YandexPracticum/src/Events.Service/Events.Application/QueryHandlers/GetTopEventsQueryHandler.cs
@@ -1,6 +1,6 @@
-using Events.Application;
 using Events.Application.Contracts.Queries;
 using Events.Application.Interfaces;
+using Events.Application.Settings;
 using Events.Domain;
 using MediatR;
 using Shared.Settings;
@@ -14,5 +14,20 @@ internal class GetTopEventsQueryHandler(
 {
     public async Task<IReadOnlyCollection<Event>> Handle(GetTopEventsQuery query, CancellationToken cancellationToken)
     {
+        var cachedEvents = await cache.GetAsync<CachedEvent[]>(CacheKeys.Top10, cancellationToken);
+        if (cachedEvents is not null)
+        {
+            return cachedEvents.Select(item => item.ToDomain()).ToArray();
+        }
+
+        var events = await eventRepository.GetTopBySoldPercentage(10, cancellationToken);
+
+        await cache.SetAsync(
+            CacheKeys.Top10,
+            events.Select(CachedEvent.FromDomain).ToArray(),
+            TimeSpan.FromSeconds(cacheSettings.TopEventsTtlSeconds),
+            cancellationToken);
+
+        return events;
     }
 }
